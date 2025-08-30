7c441579d224952dbc23009996f146e61e6d134a
johanilagan <jcilagan0918@gmail.com>
2025-08-30 19:39:46 +0800

    updated user models and login flask forms

--- researchd/forms.py
+++ researchd/forms.py
@@ -1,13 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length
+from wtforms.validators import InputRequired, Length, Email
 
 class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[InputRequired(), Length(min=3, max=150)])
+    email = StringField("Email", validators=[InputRequired(), Email(), Length(max=150)])
     password = PasswordField("Password", validators=[InputRequired(), Length(min=4)])
     submit = SubmitField("Sign In")
 
 class RegisterForm(FlaskForm):
-    username = StringField("Username", validators=[InputRequired(), Length(min=3, max=150)])
+    first_name = StringField("First Name", validators=[InputRequired(), Length(min=1, max=100)])
+    last_name = StringField("Last Name", validators=[InputRequired(), Length(min=1, max=100)])
+
+    email = StringField("Email", validators=[InputRequired(), Email(), Length(max=150)])
     password = PasswordField("Password", validators=[InputRequired(), Length(min=4)])
+
+    institution = StringField("Institution", validators=[Length(max=150)])
+    position = StringField("Position", validators=[Length(max=100)])
+
     submit = SubmitField("Register")
